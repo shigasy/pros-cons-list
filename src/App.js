24032103914cdf8e88231f74ee84bee63e7b8352
MyPoZi <kxx555u.u@gmail.com>
2019-11-06 23:25:47 +0900

    add タイトルのiput

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <section className="App-description">
           <h2 className="App-description__title">良い点、悪い点リスト</h2>
           <div className="App-description__sub-content">
-            <h5 className="App-description__sub-description">考えられる様々な選択肢について、</h5>
+            {/*<h5 className="App-description__sub-description">考えられる様々な選択肢について重みを</h5>*/}
             <a className="App-description__button--sample">サンプル</a>
             <a className="App-description__button--how-to-use">使いかた</a>
           </div>
@@ -20,7 +20,11 @@ function App() {
           <div className="content__container">
             <table align="center" border={1} className="table">
               <caption className="table__caption">
-                <span className="table__title"><strong>タイトル1</strong></span>
+                <div className="table__title">
+                  <label className="ef">
+                    <input type="text" placeholder="タイトル1"></input>
+                  </label>
+                </div>
               </caption>
               <tbody className="table__body">
               <tr className="table__header">
@@ -59,7 +63,11 @@ function App() {
             </table>
             <table align="center" border={1} className="table">
               <caption className="table__caption">
-                <span className="table__title"><strong>タイトル2</strong></span>
+                <div className="table__title">
+                  <label className="ef">
+                    <input type="text" placeholder="タイトル2"></input>
+                  </label>
+                </div>
               </caption>
               <tbody className="table__body">
               <tr className="table__header">
@@ -67,13 +75,9 @@ function App() {
                 <th className="table__header--cons" colSpan="2">悪い点</th>
               </tr>
               <tr>
-                <td className="table__data--text">
-                  <textarea className="table__data--textarea-pros" placeholder="良い点"></textarea>
-                </td>
+                <td className="table__data--text"><textarea className="table__data--textarea-pros" placeholder="良い点"></textarea></td>
                 <td className="table__data--value">1</td>
-                <td className="table__data--text">
-                  <textarea className="table__data--textarea-cons" placeholder="悪い点"></textarea>
-                </td>
+                <td className="table__data--text"><textarea className="table__data--textarea-cons" placeholder="悪い点"></textarea></td>
                 <td className="table__data--value">1</td>
               </tr>
               <tr>
@@ -92,10 +96,10 @@ function App() {
               </tr>
               <tr>
                 <td colSpan="2">
-                  <p>2</p>
+                  <p className="table__data--sum-value">10</p>
                 </td>
                 <td colSpan="2">
-                  <p>2</p>
+                  <p className="table__data--sum-value">10</p>
                 </td>
               </tr>
               </tbody>
