6a326e0bdbabbeb79ff44701473b262cf5dd3d04
MyPoZi <kxx555u.u@gmail.com>
2019-11-06 21:02:25 +0900

    add テーブルヘッダーの色

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './App.css';
 import './components/Table.css'
+import './components/Button.css'
 
 function App() {
   return (
       <div className="App">
         <section className="App-description">
-          <h1 className="App-description__title">良い点、悪い点リスト</h1>
+          <h2 className="App-description__title">良い点、悪い点リスト</h2>
           <div className="App-description__sub-content">
-            <h2 className="App-description__sub-description">考えられる様々な選択肢について、</h2>
-            <button className="App-description__button--sample">サンプル</button>
-            <button className="App-description__button--how-to-use">使い方</button>
+            <h5 className="App-description__sub-description">考えられる様々な選択肢について、</h5>
+            <a className="App-description__button--sample">サンプル</a>
+            <a className="App-description__button--how-to-use">使いかた</a>
           </div>
         </section>
         <section className="content">
           <div className="content__container">
             <table align="center" border={1} className="table">
               <caption className="table__caption">
-                <p className="table__title">タイトル1</p>
+                <span className="table__title"><strong>タイトル1</strong></span>
               </caption>
               <tbody className="table__body">
               <tr className="table__header">
@@ -25,7 +26,7 @@ function App() {
                 <th className="table__header--cons" colSpan="2">悪い点</th>
               </tr>
               <tr>
-                <td className="table__data--text">てすとてすとてすとてすと</td>
+                <td className="table__data--text">てすとてすと</td>
                 <td className="table__data--value">1</td>
                 <td className="table__data--text">てすおてすとてすとてｓつおてすおてすとてすとてｓつお</td>
                 <td className="table__data--value">1</td>
@@ -48,7 +49,7 @@ function App() {
             </table>
             <table align="center" border={1} className="table">
               <caption className="table__caption">
-                <p className="table__title">タイトル1</p>
+                <span className="table__title"><strong>タイトル2</strong></span>
               </caption>
               <tbody className="table__body">
               <tr className="table__header">
