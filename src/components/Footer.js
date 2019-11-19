9189d0d19b40d8198473632a6d56df66f3bcb589
MyPoZi <kxx555u.u@gmail.com>
2019-11-19 21:34:54 +0900

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => (
+    <div className="footer">
+      <p className="footer__text">開発者: <a href="https://twitter.com/mypozi555"><img className="footer__img" src="Twitter_Logo_250x250.png" />しがしぃ（@MyPoZi555）</a></p>
+    </div>
+)
+
+export default Footer
