78bd938a97ae5e995b23da68ae10e3fbf6b6f030
MyPoZi <kxx555u.u@gmail.com>
2019-11-21 10:05:18 +0900

    Updates

---
+++ precache-manifest.f2420de4fb831c14875f34cacef351c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c3cb58a4b83f5b158104d1593649ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdbb768da9effa4a9d79",
+    "url": "/static/css/main.fe892c7c.chunk.css"
+  },
+  {
+    "revision": "94461cb74f93f7919045",
+    "url": "/static/js/2.68ba8041.chunk.js"
+  },
+  {
+    "revision": "fdbb768da9effa4a9d79",
+    "url": "/static/js/main.b142cd16.chunk.js"
+  },
+  {
+    "revision": "51abe1cd55167aa94667",
+    "url": "/static/js/runtime-main.51f12b68.js"
+  }
+]);
