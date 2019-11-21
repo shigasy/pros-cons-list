bf3d74357237243133633b861ef8640b83f69bb5
MyPoZi <kxx555u.u@gmail.com>
2019-11-21 16:16:32 +0900

    add footerのimgにalt追加

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 
 const Footer = () => (
     <div className="footer">
-      <p className="footer__text">開発者: <a href="https://twitter.com/mypozi555"><img className="footer__img" src="Twitter_Logo_250x250.png" />しがしぃ（@MyPoZi555）</a></p>
+      <p className="footer__text">開発者: <a href="https://twitter.com/mypozi555"><img className="footer__img" alt="Twitterアイコン" src="Twitter_Logo_250x250.png" />しがしぃ（@MyPoZi555）</a></p>
     </div>
 )
 
